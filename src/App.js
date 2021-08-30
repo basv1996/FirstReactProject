4ab66b08561b2995fe3962f9c5521770bae42627
basv1996 <bas.vugts@gmail.com>
2021-08-30 14:56:32 +0200

    css, tweet component & javascript added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Tweet from './Tweet';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [users, setUsers] = useState([
+    {name: 'Bas', message: 'my message number 1'},
+    {name: 'jelle', message: 'my mes # 1'},
+    {name: 'User 3', message: 'my Tweety message'},
+    
+  ])
+
+  return(
+    <div className='app'>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+     
     </div>
   );
 }
---
+++ src/Tweet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tweet({name, message}){
+    return(
+        <div className="tweet">
+            <h3>{name}</h3>
+            <p>{message}</p>
+            <h3>Number of likes</h3>
+        </div>
+    )
+}
+
+export default Tweet;
